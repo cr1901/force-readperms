a23f5e5a465bb7ab412e6f49fb355259c7be67a7
William D. Jones <thor0505@comcast.net>
2024-07-23 15:17:52 -0400

    Refactor script to find a shared main setfacl loop.

--- force-readperms.py
+++ force-readperms.py
@@ -13,6 +13,35 @@ from pwd import getpwnam
 logger = logging.getLogger("force-readperms")
 
 
+def walk_and_examine_dirs(dir_root, username, err_list):
+    username_uid = getpwnam(username).pw_uid
+    def collect_errors(f):
+        fn = pathlib.Path(f.filename)
+
+        if isinstance(f, NotADirectoryError):
+            assert False, "Single-file support not yet implemented"
+
+        # Figure out whether the directory itself doesn't have read
+        # permissions, or the parent doesn't have execute permissions.
+        try:
+            dirinfo = os.stat(fn)
+            owner = dirinfo.st_uid
+            mode = dirinfo.st_mode
+        except PermissionError:
+            logger.debug(f" Do not have permissions to read parent directory {str(fn.parent)}")
+            err_list.append(str(fn.parent))
+        else:
+            if owner == username_uid and ((mode & 0o400) == 0):
+                # In POSIX ACLs, owner perms override named user perms.
+                logger.info(f" {username} owns {str(fn)}, adding read perms via os.chmod")
+                os.chmod(fn, dirinfo.st_mode | 0o400, follow_symlinks=False)
+            else:
+                err_list.append(str(fn))
+
+    for _ in os.walk(dir_root, onerror=collect_errors):
+        pass
+
+
 def walk_dirs_and_examine_files(dir_root, username, err_list):
     username_uid = getpwnam(username).pw_uid
 
@@ -28,6 +57,7 @@ def walk_dirs_and_examine_files(dir_root, username, err_list):
                 mode = fileinfo.st_mode
 
                 if owner == username_uid and ((mode & 0o400) == 0):
+                    # In POSIX ACLs, owner perms override named user perms.
                     logger.info(f" {username} owns {str(dirn / f)}, adding read perms via os.chmod")
                     os.chmod(dirn / f, mode | 0o400, follow_symlinks=False)
                     continue
@@ -50,6 +80,42 @@ def walk_dirs_and_examine_files(dir_root, username, err_list):
                 err_list.append(str(dirn / f))
 
 
+def setacl_loop(collector, username, limit):
+    err_list = []
+    arg_buf = []
+    loop_count = 0
+
+    collector(username, err_list)
+
+    loop_count = 0
+    while err_list and loop_count < limit:
+        base_cmd = ["sudo", "setfacl", "-m", f"user:{username}:rX"]
+        arg_buf = []
+        count = 0
+        for f in err_list:
+            count += len(f)
+            if count > 4096:
+                base_cmd.extend(arg_buf)
+                logger.info(" Running {}".format(" ".join(base_cmd)))
+                subprocess.run(base_cmd)
+
+                base_cmd = ["sudo", "setfacl", "-m", f"user:{username}:rX"]
+                arg_buf = []
+                count = 0
+            else:
+                arg_buf.append(f)
+
+        if arg_buf:
+            base_cmd.extend(arg_buf)
+            logger.info(" Running {}".format(" ".join(base_cmd)))
+            subprocess.run(base_cmd)
+
+        err_list.clear()
+        collector(username, err_list)
+
+        loop_count += 1
+
+
 def main(dir_root, username, limit, verbose):
     username_uid = getpwnam(username).pw_uid
     if verbose:
@@ -58,107 +124,18 @@ def main(dir_root, username, limit, verbose):
         loglevel = logging.INFO
 
     logging.basicConfig(level=loglevel)
-    single_file = 0
-
-    err_list = []
-    def collect_errors(f):
-        fn = pathlib.Path(f.filename)
-
-        if isinstance(f, NotADirectoryError):
-            nonlocal single_file
-            logger.debug(" {} is a file, not a directory".format(f.filename))
-            single_file = str(fn)
-
-        # Figure out whether the directory itself doesn't have read
-        # permissions, or the parent doesn't have execute permissions.
-        try:
-            dirinfo = os.stat(fn)
-            owner = dirinfo.st_uid
-            mode = dirinfo.st_mode
-        except PermissionError:
-            logger.debug(f" Do not have permissions to read parent directory {str(fn.parent)}")
-            err_list.append(str(fn.parent))
-        else:
-            if owner == username_uid and ((mode & 0o400) == 0):
-                # In POSIX ACLs, owner perms override named user perms.
-                logger.info(f" {username} owns {str(fn)}, adding read perms via os.chmod")
-                os.chmod(fn, dirinfo.st_mode | 0o400, follow_symlinks=False)
-            else:
-                err_list.append(str(fn))
 
     logger.debug(" Finding directories which need read permissions forced")
-    for _ in os.walk(dir_root, onerror=collect_errors):
-        pass
+    setacl_loop(lambda u, e: walk_and_examine_dirs(dir_root, u, e), username, limit)  
 
-    if single_file:
-        cmd = ["sudo", "setfacl", "-m", f"user:{username}:rX", single_file]
-        logger.info(" Running {}".format(" ".join(cmd)))
-        subprocess.run(cmd)
-        sys.exit(0)
-
-
-    arg_buf = []
-    loop_count = 0
-    while err_list and loop_count < limit:
-        base_cmd = ["sudo", "setfacl", "-m", f"user:{username}:rX"]
-        arg_buf = []
-        count = 0
-        for f in err_list:
-            count += len(f)
-            if count > 4096:
-                base_cmd.extend(arg_buf)
-                logger.info(" Running {}".format(" ".join(base_cmd)))
-                subprocess.run(base_cmd)
-
-                base_cmd = ["sudo", "setfacl", "-m", f"user:{username}:rX"]
-                arg_buf = []
-                count = 0
-            else:
-                arg_buf.append(f)
-
-        if arg_buf:
-            base_cmd.extend(arg_buf)
-            logger.info(" Running {}".format(" ".join(base_cmd)))
-            subprocess.run(base_cmd)
-
-        err_list.clear()
-        for _ in os.walk(dir_root, onerror=collect_errors):
-            pass
-
-        loop_count += 1
-
-    assert len(err_list) == 0
+    # if single_file:
+    #     cmd = ["sudo", "setfacl", "-m", f"user:{username}:rX", single_file]
+    #     logger.info(" Running {}".format(" ".join(cmd)))
+    #     subprocess.run(cmd)
+    #     sys.exit(0)
 
     logger.debug(" Finding files which need read permissions forced")
-    walk_dirs_and_examine_files(dir_root, username, err_list)
-
-    loop_count = 0
-    while err_list and loop_count < limit:
-        base_cmd = ["sudo", "setfacl", "-m", f"user:{username}:rX"]
-        arg_buf = []
-        count = 0
-        for f in err_list:
-            count += len(f)
-            if count > 4096:
-                base_cmd.extend(arg_buf)
-                logger.info(" Running {}".format(" ".join(base_cmd)))
-                subprocess.run(base_cmd)
-
-                base_cmd = ["sudo", "setfacl", "-m", f"user:{username}:rX"]
-                arg_buf = []
-                count = 0
-            else:
-                arg_buf.append(f)
-
-        if arg_buf:
-            base_cmd.extend(arg_buf)
-            logger.info(" Running {}".format(" ".join(base_cmd)))
-            subprocess.run(base_cmd)
-
-        err_list.clear()
-        walk_dirs_and_examine_files(dir_root, username, err_list)
-
-        loop_count += 1   
+    setacl_loop(lambda u, e: walk_dirs_and_examine_files(dir_root, u, e), username, limit)        
 
 
 if __name__ == "__main__":
